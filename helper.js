0662fe6e9fa656198a67204e37fec97afa8bbf92
Daniel Thompson <danl_thompson@hotmail.com>
2022-09-30 16:26:09 -0400

    tested on MacOS

--- helper.js
+++ helper.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 
   var s = process.argv[2].trim();
+  var outpath = "."
+  if (process.argv[3])
+    outpath = process.argv[3].trim();
+
   const fileOut = "reaper-render-config.txt";
   fs.writeFileSync(fileOut,s)
-  fs.appendFileSync(fileOut,'\n<CONFIG\n OUTPATH "C:/Users/UUSE Team/Desktop/this weeks service"\n NORMALIZE 3 -14.0 0\n BRICKWALL 1 -2\n <OUTFMT\n    ');
+  fs.appendFileSync(fileOut,'\n<CONFIG\n OUTPATH '+outpath+'\n NORMALIZE 3 -14.0 0\n BRICKWALL 1 -2\n <OUTFMT\n    ');
   fs.appendFileSync(fileOut,"UE1GRgMAAAACAAAAAAgAAAIAAACAAAAAgAcAADgEAAAAAPBBAQAAAF8AAAAAAA==");
                            //UE1GRgMAAAACAAAAAAgAAAIAAACAAAAAgAcAADgEAAAAAPBBAQAAAF8AAAAAAA==  identical
   fs.appendFileSync(fileOut,"\n >\n>\n")
--- start-norm.bat
+++ start-norm.bat
@@ -1,6 +1,6 @@
 echo normalizing volume on %1
 set REAPER_PATH=C:\"Program Files\REAPER (x64)"\Reaper
-node helper.js %1 
+node helper.js %1 "C:/Users/UUSE Team/Desktop/this weeks service"
 start %REAPER_PATH% -batchconvert reaper-render-config.txt
 pause 
 type reaper-render-config.txt

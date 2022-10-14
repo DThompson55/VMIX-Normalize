a14a031eecad8b85f97714fa0f824aa3fe136d2c
Daniel Thompson <danl_thompson@hotmail.com>
2022-10-14 10:58:26 -0400

    udpated batch

--- start-norm.bat
+++ start-norm.bat
@@ -2,7 +2,7 @@ echo normalizing volume on %1
 set REAPER_PATH=C:\"Program Files\REAPER (x64)"\Reaper
 node helper.js %1 %2 %3
 start %REAPER_PATH% -batchconvert reaper-render-config.txt
-pause 
-type reaper-render-config.txt
-type reaper-render-config.txt.log
-pause
+rem pause 
+rem type reaper-render-config.txt
+rem type reaper-render-config.txt.log
+rem pause
